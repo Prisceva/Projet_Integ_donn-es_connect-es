dd03b9ce52a1eb7ebe0bcc19f299696e09b7a395
mickail06 <mickail06@hotmail.fr>
2019-11-05 19:52:21 +0100

    [+] Adding css [~] removed unused variables/lines [~] fixed some display
    -Retirer une ligne en trop
    - rajouter "0" devant le code postal
    - les codes postales a 3 chiffre fonctione correctement
    -  debut du css
    - ergonomie et meilleur rendu visuel

--- RequeteAsso.js
+++ RequeteAsso.js
@@ -14,11 +14,10 @@ function BuildResultAsso(elementID, handicap_asso, dep) {
 			var commune_asso= new Array;
 			var lien_asso= new Array;
 			var text_asso="";
-			text_asso = text_asso + "<tr><th>Nom</th><th>Commune</th><th>Site web</th></tr>";
 				
 			for (i = 0; i < result_Assos.results.length; i++) {
 				if (result_Assos.results[i]["departement"] ==dep && result_Assos.results[i]["handicap_principal"] ==handicap_asso) {
-					text_asso=text_asso+ "<tr>"+"<td>"+result_Assos.results[i]["nom"]+"</td>" + "<td>"+result_Assos.results[i]["commune"]+"</td>" +"<td>"+result_Assos.results[i]["lien_site_onisepfr"]+"</td>" +"</tr>";
+					text_asso=text_asso+ "<tr>"+"<td>"+result_Assos.results[i]["nom"]+"</td>" + "<td>"+result_Assos.results[i]["commune"]+"</td>" +"<td>"+ result_Assos.results[i]["lien_site_onisepfr"]+"</td>" +"</tr>";
 				}
 			};
 			var associations = document.getElementById(elementID);
